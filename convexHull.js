46e28ae068bc4215421b18acd9a87cdb72722b57
Lucas <lml@cin.ufpe.br>
2017-10-24 22:33:22 -0300

    desistencia de log(n)

--- convexHull.js
+++ convexHull.js
@@ -4,7 +4,6 @@ var convexHull = {
 	},
 	
 	insert: function(hull, newpoint) {
-		debugger;
 		if (hull.length < 2) { //trivial
 			hull.push(newpoint);
 		} else if (hull.length === 2) {	//special case
@@ -16,76 +15,32 @@ var convexHull = {
 				hull.splice(1,0,newpoint);
 			}
 		} else {
-			/*
-			//checking if the point is inside or outside the present hull
-			//finding a subhull which must contain the point if the main hull contains it
-			var within = true;
-			var subhull = hull.slice();
-			while (subhull.length > 3) {
-				var i = Math.floor(subhull.length/2);
-				var lpd = this.line_point_distance(subhull[0], subhull[Math.floor(subhull.length/2)], newpoint);
-				if (this.line_point_distance(subhull[0], subhull[Math.floor(subhull.length/2)], newpoint) > 0) {
-					subhull.splice(Math.floor(subhull.length/2) + 1,subhull.length);
-				} else {
-					subhull.splice(1,Math.floor(subhull.length/2) - 1);
-				}
-			}
-			//checking if the subhull does contain the point
-			within = within && (this.line_point_distance(subhull[0], subhull[1], newpoint) > 0);
-			within = within && (this.line_point_distance(subhull[1], subhull[2], newpoint) > 0);
-			within = within && (this.line_point_distance(subhull[2], subhull[0], newpoint) > 0);
-			
-			//within the hull? nice, bye
-			if (within) return;*/
 			
 			//calculating tangent points
-			var counter = 0;
-			var left_tan = 0;
+			var tracker = 0;
+			var left_tan = -1, right_tan = -1;
+			var ltnf = true, rtnf = true;
 			var lpd_prior = this.line_point_distance(newpoint, hull[0], hull[hull.length - 1]);
 			var lpd_next  = this.line_point_distance(newpoint, hull[0], hull[1]);
-			var stepsize = 1;//Math.max(Math.floor(hull.length/2), 1);
 			
-			while (Math.max(lpd_prior, lpd_next) > 0 && counter <= hull.length) {
-				//in case we find the other tangent
-				//if(lpd_prior < 0 && lpd_next < 0) {
-				//	counter = 0;
-				//}
-				
-				//if (lpd_prior > 0) {
-					left_tan = (left_tan + hull.length - stepsize)%hull.length;
-				//} else {
-				//	left_tan = (left_tan               + stepsize)%hull.length;
-				//}
-				lpd_prior = this.line_point_distance(newpoint, hull[left_tan], hull[(left_tan + hull.length - 1)%hull.length]);
-				lpd_next  = this.line_point_distance(newpoint, hull[left_tan], hull[(left_tan               + 1)%hull.length]);
-				counter += stepsize;
-				stepsize = Math.max(Math.floor(stepsize/2), 1);
+			while (ltnf || rtnf) {
+				if (lpd_prior > 0 && lpd_next > 0) {
+					rtnf = false;
+					right_tan = tracker;
+				} else if (lpd_prior < 0 && lpd_next < 0) {
+					ltnf = false;
+					left_tan = tracker;
+				}
+				tracker += 1;
+				if(tracker === hull.length) break;
+				lpd_prior = this.line_point_distance(newpoint, hull[tracker], hull[(tracker + hull.length - 1)%hull.length]);
+				lpd_next  = this.line_point_distance(newpoint, hull[tracker], hull[(tracker               + 1)%hull.length]);
 			}
 			
 			//within the hull? nice, bye
-			if (counter > hull.length) return;
+			if (left_tan === -1 || right_tan === -1) return;
 			
-			var right_tan = 0;
-			lpd_prior = this.line_point_distance(newpoint, hull[0], hull[hull.length - 1]);
-			lpd_next  = this.line_point_distance(newpoint, hull[0], hull[1]);
-			stepsize = 1;//Math.max(Math.floor(hull.length/2), 1);
-			
-			while (Math.min(lpd_prior, lpd_next) < 0) {
-				//if(lpd_prior > 0 && lpd_next > 0) {
-				//	counter = 0;
-				//}
-				
-				//if (lpd_prior < 0) {
-					right_tan = (right_tan + hull.length - stepsize)%hull.length;
-				//} else {
-				//	right_tan = (right_tan               + stepsize)%hull.length;
-				//}
-				lpd_prior = this.line_point_distance(newpoint, hull[right_tan], hull[(right_tan + hull.length - 1)%hull.length]);
-				lpd_next  = this.line_point_distance(newpoint, hull[right_tan], hull[(right_tan               + 1)%hull.length]);
-				stepsize = Math.max(Math.floor(stepsize/2), 1);
-			}
-			
-			//this.inserting our point and removing those which are now within the hull
+			//inserting our point and removing those which are now within the hull
 			if (left_tan < right_tan) {
 				hull.splice(left_tan + 1, right_tan - left_tan - 1, newpoint);
 			} else {
